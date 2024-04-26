7b0fe11f5bd3b99b5b06d669222b2057feef639d
Crislarr <larroussecristian@outlook.com>
2024-04-26 18:40:39 -0300

    Agrega resolución de tarea 4

--- tarea-4.js
+++ tarea-4.js
@@ -5,3 +5,71 @@
 // 2. obtener el número más pequeño y mostrarlo en un <em> pre-creado con el texto "El número más pequeño es..."
 // 3. obtener el número más grande y mostrarlo en un <em> pre-creado con el texto "El número más grande es..."
 // 4. obtener el número que más se repite y mostrarlo en un <em> pre-creado con el texto "El número más frecuente es..."
+
+const $numerosNodeList = document.querySelectorAll("li");
+const numerosArray = convertirNodeListAArray($numerosNodeList);
+
+mostrarResultados();
+
+function convertirNodeListAArray(nodeList) {
+  const array = [];
+
+  for (let i = 0; i < nodeList.length; i++) {
+    array.push(Number(nodeList[i].innerText));
+  }
+  return array;
+}
+
+function calcularMayor(array) {
+  let mayor = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > mayor) {
+      mayor = array[i];
+    }
+  }
+  return mayor;
+}
+
+function calcularMenor(array) {
+  let menor = array[0];
+
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < mayor) {
+      menor = array[i];
+    }
+  }
+
+  return menor;
+}
+
+function encontrarModa(array) {
+  let moda;
+  let frecuencia = 0;
+
+  for (let i = 0; i < array.length; i++) {
+    let counter = 0;
+
+    for (let j = 0; j < array.length; j++) {
+      if (array[j] === array[i]) {
+        counter++;
+      }
+    }
+    if (counter > frecuencia) {
+      moda = array[i];
+      frecuencia = counter;
+    }
+  }
+
+  return `${moda} y se repite ${frecuencia} veces`;
+}
+
+function mostrarResultados() {
+  const $mayor = document.querySelector("#mayor");
+  const $menor = document.querySelector("#menor");
+  const $moda = document.querySelector("#moda");
+
+  $mayor.textContent = `El mayor número es ${calcularMayor(numerosArray)}`;
+  $menor.textContent = `El menor número es ${calcularMenor(numerosArray)}`;
+  $moda.textContent = `El número que más se repite es ${encontrarModa(numerosArray)}`;
+}
